58662b89b623abc97a23dd85bd14cfd5f803f291
Batuhan APAYDIN <bapaydin67@gmail.com>
2016-01-28 22:16:44 +0200

    visitcallback

---
+++ src/main/java/com/mycompany/jsf_findcomponent_visitcallback/Bean.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.jsf_findcomponent_visitcallback;
+
+import com.sun.faces.component.visit.FullVisitContext;
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.component.UIViewRoot;
+import javax.faces.component.visit.VisitCallback;
+import javax.faces.component.visit.VisitContext;
+import javax.faces.component.visit.VisitResult;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author Batuhan
+ */
+@ManagedBean
+@ViewScoped
+public class Bean implements Serializable {
+
+    public void findAndSetValue(){
+        UIInput inputText = (UIInput) findComponent("findInputText");
+        inputText.setValue("Setting value From bean");
+    }
+    
+    public UIComponent findComponent(final String id) {
+
+        FacesContext context = FacesContext.getCurrentInstance();
+        UIViewRoot root = context.getViewRoot();
+        final UIComponent[] found = new UIComponent[1];
+
+        root.visitTree(new FullVisitContext(context), new VisitCallback() {
+            @Override
+            public VisitResult visit(VisitContext context, UIComponent component) {
+                if (component.getId().equals(id)) {
+                    found[0] = component;
+                    return VisitResult.COMPLETE;
+                }
+                return VisitResult.ACCEPT;
+            }
+        });
+
+        return found[0];
+
+    }
+}
